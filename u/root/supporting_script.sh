33cb60f5e5b8418eefb72de385f87f3ecaa38ac4
rconjoe <root@trog.codes>
2025-03-20 21:41:36 +0000

    sync 2025-03-20 21:41:36

--- u/root/supporting_script.sh
+++ u/root/supporting_script.sh
@@ -8,36 +8,24 @@ token=$(curl -s -H "Authorization: Bearer $WM_TOKEN" \
   "$BASE_INTERNAL_URL/api/w/$WM_WORKSPACE/variables/get_value/u/root/plentiful_github" | jq -r .)
 date=$(date +%Y-%m-%d)
 
-# Install windmill-cli globally
 npm i -g windmill-cli
 
-# Clone the repository
 git clone https://rconjoe:$token@github.com/rconjoe/mill.trog.codes.git
 cd mill.trog.codes
 
-# Configure Git user name and email
 git config user.name "rconjoe"
 git config user.email "root@trog.codes"
 
-# Add Windmill workspace
 wmill workspace add tonka tonka https://mill.trog.codes --token "$WM_TOKEN"
 
-# Sync pull
 wmill sync pull --yes
 
-# Check for changes before committing
-if ! git diff --quiet --exit-code; then
-  # Get the current date
-  date=$(date "+%Y-%m-%d %H:%M:%S")
+date=$(date "+%Y-%m-%d %H:%M:%S")
 
-  # Commit changes
-  git commit -am "sync $date"
+git add .
+git commit -m "sync $date"
 
-  # Push changes
-  git push https://rconjoe:$token@github.com/rconjoe/mill.trog.codes.git master
-  echo "Changes committed and pushed successfully."
-else
-  echo "No changes to commit."
-fi
+# Push changes
+git push https://rconjoe:$token@github.com/rconjoe/mill.trog.codes.git master
 
 echo "Script finished."
