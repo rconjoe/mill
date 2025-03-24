47631202947a95ab16b6df4df1aaeb9d7dc5e40b
rconjoe <root@trog.codes>
2025-03-24 19:00:17 +0000

    sync 2025-03-24 19:00:17

--- f/db/test_import_types.ts
+++
@@ -1,6 +0,0 @@
-// there are multiple modes to add as header: //nobundling //native //npm //nodejs
-// https://www.windmill.dev/docs/getting_started/scripts_quickstart/typescript#modes
-
-import { DB } from "/f/db/test_export_types";
-
-const db: DB = {};
